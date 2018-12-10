713a2cce4054d4add9ca3672eb67014823dc37b7
DemonsL <245576908@qq.com>
2018-12-10 11:01:23 +0800

    Scrapy_tag_v1.0
    Scrapy_tag_v1.0 Update

--- api_scrapy/product_dynamic_timer.py
+++ api_scrapy/product_dynamic_timer.py
@@ -1,28 +1,43 @@
 # coding:utf-8
-
+import sys
+sys.path.append('../')
+import logging
 import datetime
 from common_methods import db_method
 from common_methods import common
+from common_methods import except_method
 from api_scrapy import product_dynamic
 from data_product import get_product_dynamic_data
 
-def get_asins():
+logging.basicConfig(level=logging.DEBUG,
+                    filename='dynamic_info.log',
+                    format='%(asctime)s-%(levelname)s/%(module)s(%(lineno)d): %(message)s')
+
+def get_asin_and_platform():
     session = db_method.DBSession()
-    asins = session.query(db_method.Product.asin).all()
+    asin_platforms = session.query(db_method.Product.asin, db_method.Product.platform).all()
     session.close()
-    asin_list = [asin[0] for asin in asins]
-    return asin_list
+    return asin_platforms
 
 def scrapy_timer():
-    asin_list = get_asins()
-    for asin in asin_list:
-        dynamic_param = {'asin': asin}
+    asin_platforms = get_asin_and_platform()
+    print('asins:%s' % asin_platforms)
+    for asin_platform in asin_platforms:
+        dynamic_param = {
+            'asin': asin_platform[0],
+            'platform': asin_platform[1]
+        }
         dynamic_today = get_product_dynamic_data(dynamic_param)
+        # logging.info('dynamic_today:%s' % dynamic_today)
         if not dynamic_today:
             try:
-                common.scrapy_data(asin, product_dynamic.get_dynamic_info)
-            except:
-                pass
+                common.scrapy_data(dynamic_param.get('asin'),
+                                   dynamic_param.get('platform'),
+                                   product_dynamic.get_dynamic_info)
+            except except_method.NotFoundError as e:
+                logging.info('NotFoundError: %s' % e)
+            except Exception as e:
+                logging.info('Error: %s' % e)
 
 # 定时执行抓取页面信息
 def set_timer(timer):
@@ -36,11 +51,12 @@ def set_timer(timer):
         else:
             if flag == 1:
                 timer = datetime.datetime.fromisoformat(timer)
-                timer += datetime.timedelta(days=1)
+                timer += datetime.timedelta(hours=8)
                 flag = 0
 
 
-if __name__ == "__main__":
-    timer = datetime.datetime(2018, 9, 4, 10, 29)
+if  __name__ == "__main__":
+    print('run...')
+    timer = datetime.datetime(2018, 12, 10, 9, 56)
     timer = timer.strftime('%Y-%m-%d %H:%M')
     set_timer(timer)
--- api_scrapy/product_info.py
+++ api_scrapy/product_info.py
@@ -1,31 +1,23 @@
 # coding= utf-8
-
-import time
+from bs4 import BeautifulSoup
 from common_methods import db_method
 
-            
-def get_product_info(asin, driver):
+
+def get_product_info(asin, platform, browser):
     """
     通过asin抓取商品信息
     :param asin: 商品编号
     :param driver: 调用驱动抓取信息
     :return: 返回抓取的商品信息
     """
-    runtime = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
-    product_name = driver.find_element_by_xpath('//*[@id="productTitle"]').text
-    img = driver.find_element_by_id('landingImage')
-    imglink = img.get_attribute('src') 
-    print(imglink)  
-    complete = '1'
-    asin = asin.replace(" ","")
-    asin = asin.replace("\n","")
-    asin = asin.replace("\t","")
+    soup = BeautifulSoup(browser.page_source, 'lxml')
+    product_name = get_prod_title(soup)
+    img_link = get_image_link(soup)
     params_dict = {
-        'asin': asin,
-        'prod_title': product_name,
-        'prod_image': imglink,
-        'is_complete': complete,
-        'time': runtime
+        'asin': asin.strip(),
+        'platform': platform.strip(),
+        'prod_title': product_name.strip(),
+        'prod_image': img_link
     }
     save_to_mysql(params_dict)
     return params_dict
@@ -37,4 +29,26 @@ def save_to_mysql(params_dict):
     session.add(product_info)
     session.commit()
     session.close()
+
+
+def get_prod_title(soup):
+    prod_title = soup.find(attrs={'id': 'productTitle'})
+    # wish 产品名称
+    if not prod_title:
+        prod_title = soup.find(attrs={'class': 'PurchaseContainer__Name-ghgluL crzchL'})
+    prod_title = prod_title.text
+    return prod_title
+
+
+def get_image_link(soup):
+    img = soup.find('img', attrs={'id': 'landingImage'})
+    if not img:
+        # 书籍图片
+        img = soup.find('img', attrs={'id': 'imgBlkFront'})
+    # wish 商品图片
+    if not img:
+        img = soup.find('img', attrs={'class': 'ProductImageContainer__StripImage-jMdYib fjlNJj'})
+    img_link = img.get('src')
+    return img_link
+
 
